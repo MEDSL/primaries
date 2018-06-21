5c7948c72615c89d3c5c87a2307527d2751c3fe6
Cameron Wimpy <wimpy@mit.edu>
2018-06-21 10:36:13 -0400

    updated to 17 states

--- code/download_VA_primary_results.R
+++
@@ -1,9 +0,0 @@
-library(data.table)
-
-#result site has downloadable csv file that contains precinct level results for all races
-
-dem_results <- fread("https://results.elections.virginia.gov/vaelections/2018%20June%20Democratic%20Primary/Site/results.csv")
-write.csv(dem_results, file = "~/../../Desktop/VA_dem_primary_by_precinct.csv")
-
-rep_results <- fread("https://results.elections.virginia.gov/vaelections/2018%20June%20Republican%20Primary/Site/results.csv")
-write.csv(rep_results, file = "~/../../Desktop/VA_rep_primary_by_precinct.csv")
